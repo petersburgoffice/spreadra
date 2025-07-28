7add6b49365745464f2464568fbc4c354a805ce1
Alexander Zverev <azverev@MacBook-Pro-Alexander.local>
2025-07-28 13:44:47 +0300

    Update version to v0.1.36 and add KnobLookAndFeel class

--- src/gui/ReverbEditor.h
+++ src/gui/ReverbEditor.h
@@ -3,6 +3,12 @@
 #include <juce_gui_basics/juce_gui_basics.h>
 #include <juce_audio_processors/juce_audio_processors.h>
 #include "../core/ReverbProcessor.h"
+#include <juce_gui_extra/juce_gui_extra.h>
+
+class KnobLookAndFeel : public juce::LookAndFeel_V4 {
+public:
+    void drawRotarySlider(juce::Graphics& g, int x, int y, int width, int height, float sliderPosProportional, float rotaryStartAngle, float rotaryEndAngle, juce::Slider& slider) override;
+};
 
 /**
  * @brief Базовый редактор (GUI) для Reverb плагина
@@ -35,6 +41,8 @@ private:
 
     // Label для отображения версии
     juce::Label versionLabel;
+
+    KnobLookAndFeel knobLookAndFeel;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(ReverbEditor)
 }; 
